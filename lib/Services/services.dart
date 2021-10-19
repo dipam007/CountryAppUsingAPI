529617fed993d4523d51d20e4e4a2bf9f729661e
Dipam Patel <61804421+dipam007@users.noreply.github.com>
2021-10-19 16:06:51 +0530

    Update services.dart

--- lib/Services/services.dart
+++ lib/Services/services.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 class MyServices{
 
   Future<List> getCountryFromApi() async{
-      var res = await Dio().get('https://restcountries.eu/rest/v2/all');
+      var res = await Dio().get('http://api.countrylayer.com/v2/all?access_key=6336548059f3d32caeb758f13162f8c2'); //https://restcountries.eu/rest/v2/all
       return res.data;
   }
 }
